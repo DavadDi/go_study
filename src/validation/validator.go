a156f1dd25299d3059b3d079a0dc6c1b5a87c9d9
DavadDi <dwh0403@163.com>
2017-02-27 15:50:07 +0800

    delete unused code

--- src/validation/validator.go
+++ src/validation/validator.go
@@ -328,25 +328,3 @@ func (mv *Validation) getValidFuns(tf reflect.StructField, tag string) map[strin
 
 	return out
 }
-
-// Not used for now
-func isEmptyValue(v reflect.Value) bool {
-	switch v.Kind() {
-	case reflect.String, reflect.Array:
-		return v.Len() == 0
-	case reflect.Map, reflect.Slice:
-		return v.Len() == 0 || v.IsNil()
-	case reflect.Bool:
-		return !v.Bool()
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	}
-
-	return reflect.DeepEqual(v.Interface(), reflect.Zero(v.Type()).Interface())
-}
