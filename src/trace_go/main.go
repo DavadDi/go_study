7c746f5aca07213abb025fb294ca04cc587fd83a
DavadDi <dwh0403@163.com>
2017-02-28 15:38:20 +0800

    add two way for trace

---
+++ src/trace_go/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Trace(s string) string {
+	fmt.Println("Entering: ", s)
+	return s
+
+}
+
+func Untrace(s string) {
+	fmt.Println("Leaving:", s)
+}
+
+// used anonymous func
+func TraceFunc(s string) func() {
+	fmt.Println("Entering: " + s)
+	return func() {
+		fmt.Println("Leaving: " + s)
+	}
+}
+
+func main() {
+    // annonymouse usage
+	// defer TraceFunc("main")()
+
+    // netsted usage
+	defer Untrace(Trace("main"))
+	fmt.Println("Hello, playground")
+
+}
