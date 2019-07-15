807e3ff6cf50dc7def2df46d44a08871e872c517
DavadDi <dwh0403@163.com>
2019-07-15 18:23:15 +0800

    add wasm adv for web

---
+++ src/wasm_adv/README.md
@@ -0,0 +1,22 @@
+# INTRO
+
+> go version go1.12 darwin/amd64
+
+## command 
+```bash
+$ GOOS=js GOARCH=wasm go build -o lib.wasm
+
+$ cp $(go env GOROOT)/misc/wasm/wasm_exec.js .
+$ go run file_server.go
+```
+
+## In Chrome
+
+* Type Addr: http://127.0.0.1:8080/wasm_exec.html
+
+## see also
+
+* [Go WebAssembly Tutorial - Building a Calculator Tutorial](https://tutorialedge.net/golang/go-webassembly-tutorial/)
+* [Golang's syscall/js js.NewCallback is undefined](https://stackoverflow.com/questions/55800163/golangs-syscall-js-js-newcallback-is-undefined)
+
+
---
+++ src/wasm_adv/lib.go
@@ -0,0 +1,42 @@
+package main
+
+import "strconv"
+
+import "syscall/js"
+
+func add(this js.Value, i []js.Value) interface{} {
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1+int2)
+
+	return int1 + int2
+}
+
+func subtract(this js.Value, i []js.Value) interface{} {
+	value1 := js.Global().Get("document").Call("getElementById", i[0].String()).Get("value").String()
+	value2 := js.Global().Get("document").Call("getElementById", i[1].String()).Get("value").String()
+
+	int1, _ := strconv.Atoi(value1)
+	int2, _ := strconv.Atoi(value2)
+
+	js.Global().Get("document").Call("getElementById", i[2].String()).Set("value", int1-int2)
+	return int1 - int2
+}
+
+func registerCallbacks() {
+	js.Global().Set("add", js.FuncOf(add))
+	js.Global().Set("subtract", js.FuncOf(subtract))
+}
+
+func main() {
+	c := make(chan struct{}, 0)
+
+	println("WASM Go Initialized")
+	// register functions
+	registerCallbacks()
+	<-c
+}
